d64da0ba6be90415b61e97df6279c326070badd0
Pratiksha-1610 <p.s16102004@gmail.com>
2026-04-15 12:08:46 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from groq import Groq
 
-st.set_page_config("PragyanAI Content Generator", layout="wide")
+st.set_page_config("ChatBot Created By Pratiksha", layout="wide")
 st.title("PragyanAI – Content Generator")
 st.image("WhatsApp Image 2025-10-29 at 13.16.56_0fce599c.jpg")
 # Get GROQ API Key
