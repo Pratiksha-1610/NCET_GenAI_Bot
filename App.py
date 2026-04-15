2cbfcf530690a5618b4d2e601199d10fd2f1ad43
Pratiksha-1610 <p.s16102004@gmail.com>
2026-04-15 10:57:27 +0530

    Update App.py

--- App.py
+++ App.py
@@ -3,4 +3,6 @@ from groq import Groq
 
 st.set_page_config("PragyanAI Content Generator", layout="wide")
 st.title("PragyanAI – Content Generator")
+st.image("WhatsApp Image 2025-10-29 at 13.16.56_0fce599c.jpg")
+
 
